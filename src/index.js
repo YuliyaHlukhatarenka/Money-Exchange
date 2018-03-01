415cb86033ea222f8d78b0ae47030dc5458788e2
Dmitry <d.gluhotorenko@gmail.com>
2018-03-01 14:05:52 +0300

    'Money-Exchange/feature:implement'

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
 // PLEASE DON'T change function name
-module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+//var ost;
+let result = [];
+const coins = [50, 25, 10, 5, 1];
+const coinsLetter = ["H", "Q", "D", "N", "P"];
+let iteration = 0;
+let arrResult = [];
+
+module.exports =
+function makeExchange(currency) {
+  if (currency > 10000) {
+    return {error: "You are rich, my friend! We don't have so much coins for exchange"}
+  }
+  arrResult = [];
+  makeExch(currency);
+  return arrResult;
+}
+
+function makeExch(currency) {
+      if (currency === 0) {
+        result = [];
+        iteration = 0;
+        return;
+      } else {
+        let currentCoin = coins[iteration];
+        let countOfCurrentCoin = Math.floor(currency / currentCoin);
+        result[iteration] = countOfCurrentCoin;
+
+        if(countOfCurrentCoin > 0) {
+          arrResult[coinsLetter[iteration]] = result[iteration];
+        }
+
+        iteration++;
+        return makeExch(currency % currentCoin);
+      }
 }
